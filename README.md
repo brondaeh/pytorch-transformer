6412fadee5c3d50a23b93ce64726a4a4e64fe296
Brandon <huangbrandon2@gmail.com>
2024-02-04 12:01:37 -0800

    created initial layers

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # pytorch-transformer
+
+## Introduction
+In this project, a transformer model is created from scratch in the PyTorch framework.
---
+++ model.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import math
+
+
+class InputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        
+        # Create a vector of shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+
+        # Apply sine to even positions
+        pe[:, 0::2] = torch.sin(position * div_term)
+
+        # Apply cosine to odd positions
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)    # (1, seq_len, d_model)
+
+        # PEs are not learned parameters of the model, but these tensors must be stored in a buffer to be reused for different inputs
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)  # requires_grade_(False) used so the tensor is not learned by the model
+        return self.dropout(x)
+    
+
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps                              # epsilon is used for numerical stability and to avoid division by 0
+        self.alpha = nn.Parameter(torch.ones(1))    # learned parameter that is multiplied
+        self.bias = nn.Parameter(torch.zeros(1))    # learned parameter that is added
+
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim=True)
+        std = x.std(dim = -1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+    
+
